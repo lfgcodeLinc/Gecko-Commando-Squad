231461f8aa2531088c6daaed436601b4cfa6e186
Dan Herbst <nr5go@yahoo.com>
2014-11-22 23:19:18 -0500

    Change to Angularjs

---
+++ itisforgirls/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gccdApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gccdApp
+ */
+angular.module('gccdApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
