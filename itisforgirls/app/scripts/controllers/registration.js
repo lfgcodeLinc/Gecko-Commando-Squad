045935256ee184c9c3fdac2d2042eb7a5570ba4e
Patrick Brown <patrick@brownsweb.net>
2014-11-23 04:01:40 -0500

    Registration page

--- itisforgirls/app/scripts/controllers/registration.js
+++ itisforgirls/app/scripts/controllers/registration.js
@@ -8,10 +8,37 @@
  * Controller of the codelincTestApp
  */
 angular.module('itisforgirls')
-  .controller('RegistrationCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('RegistrationCtrl', ['$scope', function($scope) {
+      $scope.master = {};
+
+      $scope.update = function(user) {
+         var uName = user.name;
+         var uEmail = user.email;
+
+          /*function go() {
+              var userEmail = uEmail;
+              checkIfUserExists(userEmail);
+          }
+
+          var USERS_LOCATION = 'https://SampleChat.firebaseIO-demo.com/users';
+
+          function userExistsCallback(userEmail, exists) {
+              if (exists) {
+                  alert('user ' + userId + ' exists!');
+              } else {
+                  alert('user ' + userId + ' does not exist!');
+              }
+          }
+
+// Tests to see if /users/<userEmail> has any data.
+          function checkIfUserExists(userEmail) {
+              var usersRef = new Firebase(USERS_LOCATION);
+              usersRef.child(userEmail).once('value', function(snapshot) {
+                  var exists = (snapshot.val() !== null);
+                  userExistsCallback(userEmail, exists);
+              });
+          }
+*/
+      };
+
+    }]);
